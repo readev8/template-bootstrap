72890aead7fa03ac662e69f3fe3c6a498217e32c
Rea <readev8@gmail.com>
2026-04-05 00:15:36 +0700

    fix layout navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,19 +3,27 @@
 
   $(document).ready(function () {
     var $navbar = $('.glass-navbar');
+    
     if ($navbar.length === 0) {
+      alert('ERROR: Navbar tidak ditemukan!');
       return;
     }
 
     function updateNavbarScroll() {
-      if ($(window).scrollTop() > 20) {
+      var scrollTop = $(window).scrollTop();
+      
+      if (scrollTop > 20) {
         $navbar.addClass('scrolled');
+        console.log('Scrolled class ditambahkan. Scroll:', scrollTop);
       } else {
         $navbar.removeClass('scrolled');
+        console.log('Scrolled class dihapus. Scroll:', scrollTop);
       }
     }
 
     $(window).on('scroll', updateNavbarScroll);
-    updateNavbarScroll();
+    updateNavbarScroll(); // Jalankan sekali saat load
+    
+    alert('Navbar script loaded! Scroll untuk test.');
   });
 })(jQuery);
