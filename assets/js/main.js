e3a999442734e53d9f39b0ff122995411daf7193
Rea <readev8@gmail.com>
2026-04-05 05:01:20 +0700

    minor update table

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,27 @@
 (function ($) {
-  'use strict';
+  "use strict";
 
   $(document).ready(function () {
-    var $navbar = $('.glass-navbar');
-    
+    var $navbar = $(".glass-navbar");
+
     if ($navbar.length === 0) {
-      alert('ERROR: Navbar tidak ditemukan!');
+      alert("ERROR: Navbar tidak ditemukan!");
       return;
     }
 
     function updateNavbarScroll() {
       var scrollTop = $(window).scrollTop();
-      
+
       if (scrollTop > 20) {
-        $navbar.addClass('scrolled');
-        console.log('Scrolled class ditambahkan. Scroll:', scrollTop);
+        $navbar.addClass("scrolled");
+        console.log("Scrolled class ditambahkan. Scroll:", scrollTop);
       } else {
-        $navbar.removeClass('scrolled');
-        console.log('Scrolled class dihapus. Scroll:', scrollTop);
+        $navbar.removeClass("scrolled");
+        console.log("Scrolled class dihapus. Scroll:", scrollTop);
       }
     }
 
-    $(window).on('scroll', updateNavbarScroll);
+    $(window).on("scroll", updateNavbarScroll);
     updateNavbarScroll(); // Jalankan sekali saat load
-    
-    alert('Navbar script loaded! Scroll untuk test.');
   });
 })(jQuery);
